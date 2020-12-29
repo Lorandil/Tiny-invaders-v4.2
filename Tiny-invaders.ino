f599bf4be31bc41639b3a1830e1f7897a5e3b43f
Lorandil <70488869+Lorandil@users.noreply.github.com>
2020-12-29 14:11:33 +0100

    added line //#define _USE_SH1106_

--- Tiny-invaders.ino
+++ Tiny-invaders.ino
@@ -22,6 +22,8 @@
 //the code work at 16MHZ internal
 //and use ssd1306xled Library for SSD1306 oled display 128x64
 
+//#define _USE_SH1106_
+
 #include <ssd1306xled.h>
 #include "spritebank.h"
 #include "displayscore.h" // sbr
