d19d391c93dddc37acf8c5c459b79715c76dce9f
Lorandil <70488869+Lorandil@users.noreply.github.com>
2021-01-16 23:32:03 +0100

    Added support for Arduino UNO and Mega2560 for easier debugging
    Depending on the compiler constants the code will use the AdafruitSSD1306 library instead of the ATTiny-only ssd1306xled library. This enables faster develeopment cycles (and serial debugging) on Arduino UNO or Mega2560

--- Tiny-invaders.ino
+++ Tiny-invaders.ino
@@ -22,9 +22,23 @@
 //the code work at 16MHZ internal
 //and use ssd1306xled Library for SSD1306 oled display 128x64
 
+// optional support for SH1106 displays (shifted output)
 //#define _USE_SH1106_
 
-#include <ssd1306xled.h>
+#if defined(__AVR_ATtiny85__) // sbr
+  #define LEFT_RIGHT_BUTTON A0
+  #define UP_DOWN_BUTTON    A3
+  #define FIRE_BUTTON        1
+  // fast library - but only works on ATTiny85
+  #include <ssd1306xled.h>
+#else
+  #define LEFT_RIGHT_BUTTON A0
+  #define UP_DOWN_BUTTON    A3
+  #define FIRE_BUTTON       A1
+  // classic Adafruit library works on many controllers
+  #include <Adafruit_SSD1306.h>
+  Adafruit_SSD1306 display( 128, 64, &Wire, -1 );
+#endif
 #include "spritebank.h"
 #include "displayscore.h" // sbr
 #include "RLEdecompression.h" // sbr
@@ -80,12 +94,26 @@ SPACE space;  // sbr
 // fin var public
 
 void setup() {
+#if defined(__AVR_ATtiny85__)
   SSD1306.ssd1306_init();
   // not using 'pinMode()' here saves ~100 bytes of flash!
   // configure A0, A3 and D1 as input
   DDRB &= ~( ( 1 << PB5) | ( 1 << PB3 ) | ( 1 << PB1 ) );
   // configure A2 as output
   DDRB |= ( 1 << PB4 );
+#else
+  // DEBUG version on controller with serial ports
+  Serial.begin( 115200 );
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  // use 'pinMode()' for simplicity's sake... any other micro controller has enough flash :)
+  pinMode( LEFT_RIGHT_BUTTON, INPUT );
+  pinMode( UP_DOWN_BUTTON, INPUT );
+  pinMode( FIRE_BUTTON, INPUT );
+#endif
   // restore highscore from EEPROM
   initHighScoreStruct( TINY_INVADERS_EEPROM_ADDR ); // sbr
 }
@@ -145,7 +173,7 @@ NEWGAME:
       n = 0;
     }
     // exit if button pressed
-    if (digitalRead(1)==0) {bebeep(); goto BYPASS2;} // sbr
+    if (digitalRead( FIRE_BUTTON )==0) {bebeep(); goto BYPASS2;} // sbr
   }
   
 NEWLEVEL:
@@ -203,7 +231,7 @@ Bypass:
         space.frame=0;
       }
       // just read the input once
-      uint16_t input = analogRead( A0 ) - 500; // sbr
+      uint16_t input = analogRead( LEFT_RIGHT_BUTTON ) - 500; // sbr
       // right button pressed?
       //if ( (input>500)&&(input<750) ) 
       if ( input<250 ) {  // sbr
@@ -216,7 +244,7 @@ Bypass:
         if (VarPot>5) { VarPot=VarPot-6; }
       }
       // check for fire button
-      if ((digitalRead(1)==0)&&(MyShootReady==SHOOTS)){
+      if ((digitalRead( FIRE_BUTTON )==0)&&(MyShootReady==SHOOTS)){ // sbr
         Sound(200,4);
         MyShootReady=0;
         space.MyShootBall=6;
@@ -309,11 +337,13 @@ void Tiny_Flip(uint8_t render0_picture1,SPACE *space){
   // just calculate the offset once
   //space.ScrBackV=(ShipPos/14)+52;
   calcNewBackgroundOffset( space ); // sbr
-  
+
   for (y = 0; y < 8; y++)
   {
     // uncompress chunk and save next address
     render = pgm_RLEdecompress( render, chunkBuffer, 128 ); // sbr
+
+#if defined(__AVR_ATtiny85__)
     // initialize image transfer to segment 'y'
     SSD1306.ssd1306_send_command(0xb0 + y);
   #ifdef _USE_SH1106_
@@ -327,6 +357,10 @@ void Tiny_Flip(uint8_t render0_picture1,SPACE *space){
     SSD1306.ssd1306_send_command(0x10);  
   #endif    
     SSD1306.ssd1306_send_data_start();
+#else
+  // allocate a buffer in RAM
+  uint8_t *buffer = display.getBuffer() + y * 128;
+#endif
     
     for (x = 0; x < 128; x++)
     {
@@ -353,7 +387,11 @@ void Tiny_Flip(uint8_t render0_picture1,SPACE *space){
       else {
         pixels = displayZoomedText(x,y);        
       }
-      SSD1306.ssd1306_send_byte( pixels ); // sbr
+      #if defined(__AVR_ATtiny85__)
+        SSD1306.ssd1306_send_byte( pixels ); // sbr
+      #else
+        *buffer++ = pixels;
+      #endif
     } // for x
     
     if (render0_picture1 == GAME_SCREEN) {
@@ -363,9 +401,12 @@ void Tiny_Flip(uint8_t render0_picture1,SPACE *space){
         //SSD1306.ssd1306_send_data_stop(); // sbr
       }
     }
+
+  #if defined(__AVR_ATtiny85__)
     // this line appears to be optional, as it was never called during the intro screen...
     // but hey, we still have some bytes left ;)
     SSD1306.ssd1306_send_data_stop(); // sbr
+  #endif
   } // for y
   
   if (render0_picture1 == GAME_SCREEN) {
@@ -378,6 +419,10 @@ void Tiny_Flip(uint8_t render0_picture1,SPACE *space){
       }
     }
   }
+
+#if !defined(__AVR_ATtiny85__)
+  display.display();
+#endif
 }
 
 uint8_t UFOWrite(uint8_t x,uint8_t y,SPACE *space){
@@ -805,9 +850,8 @@ void showNewHighScore(SPACE *space )
       uint8_t letter = *initials;
       
       // read the analog input only once
-      uint16_t inputX = analogRead( A0 ) - 500;
-      uint16_t inputY = analogRead( A3 ) - 500;
-
+      uint16_t inputX = analogRead( LEFT_RIGHT_BUTTON ) - 500;
+      uint16_t inputY = analogRead( UP_DOWN_BUTTON ) - 500;
 
       // right/up button pressed?
       if ( ( inputX < 250 ) || ( inputY < 250 ) )
@@ -836,12 +880,12 @@ void showNewHighScore(SPACE *space )
       // display letter
       Tiny_Flip( BLANK_SCREEN,space);
     }
-    while ( digitalRead(1) );
+    while ( digitalRead( FIRE_BUTTON ) ); // sbr
     
     // ok, got it!
     bebeep();
     // wait for 'fire' released
-    while ( !digitalRead(1) );
+    while ( !digitalRead( FIRE_BUTTON ) );  // sbr
 
     // next letter
     initials++;
